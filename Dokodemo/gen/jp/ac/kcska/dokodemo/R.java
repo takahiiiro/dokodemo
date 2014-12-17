099cae4495fa9e4385a7daee11f85f2cdd09111f
立木 伸昌 <mkanami1@gmail.com>
2014-12-17 09:43:32 +0900

    クラスを作ったにゃん(はあと2

--- Dokodemo/gen/jp/ac/kcska/dokodemo/R.java
+++ Dokodemo/gen/jp/ac/kcska/dokodemo/R.java
@@ -1096,12 +1096,36 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_dokodemo_main=0x7f030017;
-        public static final int fragment_dokodemo_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_blood_main=0x7f030017;
+        public static final int activity_dokodemo_main=0x7f030018;
+        public static final int activity_food_main=0x7f030019;
+        public static final int activity_graph_main=0x7f03001a;
+        public static final int activity_home_main=0x7f03001b;
+        public static final int activity_login_main=0x7f03001c;
+        public static final int activity_medicine_main=0x7f03001d;
+        public static final int activity_show_main=0x7f03001e;
+        public static final int activity_vital_main=0x7f03001f;
+        public static final int fragment_blood_main=0x7f030020;
+        public static final int fragment_dokodemo_main=0x7f030021;
+        public static final int fragment_food_main=0x7f030022;
+        public static final int fragment_graph_main=0x7f030023;
+        public static final int fragment_home_main=0x7f030024;
+        public static final int fragment_login_main=0x7f030025;
+        public static final int fragment_medicine_main=0x7f030026;
+        public static final int fragment_show_main=0x7f030027;
+        public static final int fragment_vital_main=0x7f030028;
+        public static final int support_simple_spinner_dropdown_item=0x7f030029;
     }
     public static final class menu {
-        public static final int dokodemo_main=0x7f0c0000;
+        public static final int blood_main=0x7f0c0000;
+        public static final int dokodemo_main=0x7f0c0001;
+        public static final int food_main=0x7f0c0002;
+        public static final int graph_main=0x7f0c0003;
+        public static final int home_main=0x7f0c0004;
+        public static final int login_main=0x7f0c0005;
+        public static final int medicine_main=0x7f0c0006;
+        public static final int show_main=0x7f0c0007;
+        public static final int vital_main=0x7f0c0008;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1147,6 +1171,14 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_blood_main=0x7f0a0010;
+        public static final int title_activity_food_main=0x7f0a0011;
+        public static final int title_activity_graph_main=0x7f0a0012;
+        public static final int title_activity_home_main=0x7f0a0013;
+        public static final int title_activity_login_main=0x7f0a0014;
+        public static final int title_activity_medicine_main=0x7f0a0015;
+        public static final int title_activity_show_main=0x7f0a0016;
+        public static final int title_activity_vital_main=0x7f0a0017;
     }
     public static final class style {
         /** 
