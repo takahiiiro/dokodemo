c662b0c759d7c38b9c3837fb0e84b30c60b63f48
立木 伸昌 <mkanami1@gmail.com>
2014-12-19 10:22:07 +0900

    Oh Yeah

--- Dokodemo/gen/jp/ac/kcska/dokodemo/R.java
+++ Dokodemo/gen/jp/ac/kcska/dokodemo/R.java
@@ -16,6 +16,9 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
+    public static final class array {
+        public static final int week_names=0x7f0c0000;
+    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -981,9 +984,17 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int button=0x7f020057;
+        public static final int button_normal=0x7f020058;
+        public static final int button_pressed=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
+        public static final int item_background=0x7f02005b;
+        public static final int note=0x7f02005c;
+        public static final int orange=0x7f02005d;
+        public static final int tabb=0x7f02005e;
     }
     public static final class id {
+        public static final int RelativeLayout1=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -995,10 +1006,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05004d;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050041;
+        public static final int button2=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1010,6 +1023,7 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int gridLayout1=0x7f050044;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1024,6 +1038,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int scrollView1=0x7f050043;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1040,7 +1055,18 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
+        public static final int tab1=0x7f050045;
+        public static final int tab2=0x7f050046;
+        public static final int tab3=0x7f050047;
+        public static final int tab4=0x7f050049;
+        public static final int tab5=0x7f05004b;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f05003e;
+        public static final int textView2=0x7f05003f;
+        public static final int textView3=0x7f050040;
+        public static final int textView4=0x7f050048;
+        public static final int textView5=0x7f05004a;
+        public static final int textView6=0x7f05004c;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1097,35 +1123,41 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_blood_main=0x7f030017;
-        public static final int activity_dokodemo_main=0x7f030018;
-        public static final int activity_food_main=0x7f030019;
-        public static final int activity_graph_main=0x7f03001a;
-        public static final int activity_home_main=0x7f03001b;
-        public static final int activity_login_main=0x7f03001c;
-        public static final int activity_medicine_main=0x7f03001d;
-        public static final int activity_show_main=0x7f03001e;
-        public static final int activity_vital_main=0x7f03001f;
-        public static final int fragment_blood_main=0x7f030020;
-        public static final int fragment_dokodemo_main=0x7f030021;
-        public static final int fragment_food_main=0x7f030022;
-        public static final int fragment_graph_main=0x7f030023;
-        public static final int fragment_home_main=0x7f030024;
-        public static final int fragment_login_main=0x7f030025;
-        public static final int fragment_medicine_main=0x7f030026;
-        public static final int fragment_show_main=0x7f030027;
-        public static final int fragment_vital_main=0x7f030028;
-        public static final int support_simple_spinner_dropdown_item=0x7f030029;
+        public static final int activity_cal_main=0x7f030018;
+        public static final int activity_dokodemo_main=0x7f030019;
+        public static final int activity_food_main=0x7f03001a;
+        public static final int activity_graph_main=0x7f03001b;
+        public static final int activity_home_main=0x7f03001c;
+        public static final int activity_login_main=0x7f03001d;
+        public static final int activity_medicine_main=0x7f03001e;
+        public static final int activity_show_main=0x7f03001f;
+        public static final int activity_vital_main=0x7f030020;
+        public static final int calendar_date=0x7f030021;
+        public static final int calendar_main=0x7f030022;
+        public static final int calendar_week=0x7f030023;
+        public static final int fragment_blood_main=0x7f030024;
+        public static final int fragment_cal_main=0x7f030025;
+        public static final int fragment_dokodemo_main=0x7f030026;
+        public static final int fragment_food_main=0x7f030027;
+        public static final int fragment_graph_main=0x7f030028;
+        public static final int fragment_home_main=0x7f030029;
+        public static final int fragment_login_main=0x7f03002a;
+        public static final int fragment_medicine_main=0x7f03002b;
+        public static final int fragment_show_main=0x7f03002c;
+        public static final int fragment_vital_main=0x7f03002d;
+        public static final int show_main=0x7f03002e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03002f;
     }
     public static final class menu {
-        public static final int blood_main=0x7f0c0000;
-        public static final int dokodemo_main=0x7f0c0001;
-        public static final int food_main=0x7f0c0002;
-        public static final int graph_main=0x7f0c0003;
-        public static final int home_main=0x7f0c0004;
-        public static final int login_main=0x7f0c0005;
-        public static final int medicine_main=0x7f0c0006;
-        public static final int show_main=0x7f0c0007;
-        public static final int vital_main=0x7f0c0008;
+        public static final int blood_main=0x7f0d0000;
+        public static final int dokodemo_main=0x7f0d0001;
+        public static final int food_main=0x7f0d0002;
+        public static final int graph_main=0x7f0d0003;
+        public static final int home_main=0x7f0d0004;
+        public static final int login_main=0x7f0d0005;
+        public static final int medicine_main=0x7f0d0006;
+        public static final int show_main=0x7f0d0007;
+        public static final int vital_main=0x7f0d0008;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1170,15 +1202,18 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int btn_month_next=0x7f0a0019;
+        public static final int btn_month_prev=0x7f0a001a;
         public static final int hello_world=0x7f0a000e;
         public static final int title_activity_blood_main=0x7f0a0010;
+        public static final int title_activity_cal_main=0x7f0a0017;
         public static final int title_activity_food_main=0x7f0a0011;
         public static final int title_activity_graph_main=0x7f0a0012;
         public static final int title_activity_home_main=0x7f0a0013;
         public static final int title_activity_login_main=0x7f0a0014;
         public static final int title_activity_medicine_main=0x7f0a0015;
         public static final int title_activity_show_main=0x7f0a0016;
-        public static final int title_activity_vital_main=0x7f0a0017;
+        public static final int title_activity_vital_main=0x7f0a0018;
     }
     public static final class style {
         /** 
